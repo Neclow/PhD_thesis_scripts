f0f6f65542451e62720fa1e41652a637b1fb74e3
Neclow <neilclow24@gmail.com>
2026-01-21 19:00:05 +0000

    cosmetic updates

--- src/rtt_vs_tdi.R
+++ src/rtt_vs_tdi.R
@@ -8,18 +8,15 @@ library(TreeDist)
 library(Cairo)
 
 # ----------------------------------------------------------------------------
-# 1. Create two gene trees: B is the discordant taxon
+# 1. Create two gene trees with a discordant taxon
 # ----------------------------------------------------------------------------
 
 # Tree for Region 1: ((A,C),((B,D),E))
-# B clusters deeply with D
 tree1 <- read.tree(
   text = "((A:0.15,C:0.12):0.2,((B:0.08,D:0.1):0.12,E:0.18):0.15);"
 )
 
 # Tree for Region 2: ((A,(B,C)),(D,E))
-# B has recombined - now clusters with C instead of D
-# B has much longer branch in Region 2 (accelerated evolution)
 tree2 <- read.tree(
   text = "((A:0.1,(B:0.45,C:0.12):0.1):0.2,(D:0.14,E:0.16):0.18);"
 )
@@ -33,7 +30,7 @@ highlight_taxon <- "B"
 
 get_rtt <- function(tree) {
   root_node <- length(tree$tip.label) + 1
-  distances <- dist.nodes(tree)[1:length(tree$tip.label), root_node]
+  distances <- dist.nodes(tree)[seq_along(tree$tip.label), root_node]
   names(distances) <- tree$tip.label
   return(distances)
 }
@@ -47,11 +44,11 @@ rtt2 <- get_rtt(tree2)
 
 mci <- MutualClusteringInfo(tree1, tree2)
 
-# TDI_B = MCI(tree1, tree2) - MCI(tree1 without B, tree2 without B)
+# tdi_b = MCI(tree1, tree2) - MCI(tree1 without B, tree2 without B)
 tree1_drop <- drop.tip(tree1, highlight_taxon)
 tree2_drop <- drop.tip(tree2, highlight_taxon)
 mci_reduced <- MutualClusteringInfo(tree1_drop, tree2_drop)
-tdi_B <- mci - mci_reduced
+tdi_b <- mci - mci_reduced
 
 # ----------------------------------------------------------------------------
 # 4. Visualize as tanglegram with annotations
@@ -89,11 +86,17 @@ plot_tanglegram <- function() {
   # Add branch length labels (positioned above branches)
   edgelabels.cophylo(
     round(cophy$trees[[1]]$edge.length, 2),
-    which = "left", cex = 1.2, frame = "none", adj = c(0.5, -0.3)
+    which = "left",
+    cex = 1.2,
+    frame = "none",
+    adj = c(0.5, -0.3)
   )
   edgelabels.cophylo(
     round(cophy$trees[[2]]$edge.length, 2),
-    which = "right", cex = 1.2, frame = "none", adj = c(0.5, -0.3)
+    which = "right",
+    cex = 1.2,
+    frame = "none",
+    adj = c(0.5, -0.3)
   )
 
   mtext("Region 1", side = 3, adj = 0.15, line = 1, font = 2, cex = 1.3)
@@ -113,9 +116,15 @@ dev.off()
 # Print metrics to terminal
 cat("\n")
 cat("MCI(Tree 1, Tree 2) =", round(mci, 3), "\n")
-cat("TDI_B = MCI(Tree 1, Tree 2) - MCI(Tree 1 \\ B, Tree 2 \\ B) =",
-    round(mci, 3), "-", round(mci_reduced, 3), "=", round(tdi_B, 3), "\n")
+cat(
+  "tdi_b = MCI(Tree 1, Tree 2) - MCI(Tree 1 \\ B, Tree 2 \\ B) =",
+  round(mci, 3),
+  "-",
+  round(mci_reduced, 3),
+  "=",
+  round(tdi_b, 3),
+  "\n"
+)
 cat("RTT_B (Region 1) =", round(rtt1[highlight_taxon], 3), "\n")
 cat("RTT_B (Region 2) =", round(rtt2[highlight_taxon], 3), "\n")
 cat("\nDone\n")
-
